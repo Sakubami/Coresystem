08fa58169c3b74d6acd0379b2d5b0af7c3d7e88a
BuildTools <louiswaechter1994@gmail.com>
2023-09-12 01:05:22 +0200

    stuff to test

--- src/main/java/net/haraxx/coresystem/modules/CoreModule.java
+++ src/main/java/net/haraxx/coresystem/modules/CoreModule.java
@@ -1,7 +1,13 @@
 package net.haraxx.coresystem.modules;
 
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public abstract class CoreModule extends JavaPlugin {
+    public CoreModule module;
 
+    public CoreModule getModule() {
+        return module;
+    }
 }
