85a90c2f985abdcefddb02890b0745a074f6a300
BuildTools <louiswaechter1994@gmail.com>
2023-11-11 16:44:12 +0100

    a

--- src/main/java/net/haraxx/coresystem/commands/SpawnCommand.java
+++ src/main/java/net/haraxx/coresystem/commands/SpawnCommand.java
@@ -3,6 +3,7 @@ package net.haraxx.coresystem.commands;
 import net.haraxx.coresystem.builder.Chat;
 import net.haraxx.coresystem.permissions.Utils;
 import net.haraxx.coresystem.configs.WorldSpawnConfig;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,7 +16,8 @@ public class SpawnCommand implements CommandExecutor {
         if (command.getName().equalsIgnoreCase("spawn")) {
             Player player = (Player) commandSender;
             if (Utils.isVerified(player)) {
-                player.teleport(WorldSpawnConfig.get().getWorldSpawnLocation());
+                Location oldLoc = WorldSpawnConfig.get().getWorldSpawnLocation();
+                player.teleport(new Location(oldLoc.getWorld(), oldLoc.getBlockX() + 0.5, oldLoc.getBlockY() + 0.125, oldLoc.getBlockZ() + 0.5, 0  , 0));
                 player.sendMessage(Chat.format("Teleporting..."));
             }
         }
--- src/main/java/net/haraxx/coresystem/configs/WorldSpawnConfig.java
+++ src/main/java/net/haraxx/coresystem/configs/WorldSpawnConfig.java
@@ -1,6 +1,7 @@
 package net.haraxx.coresystem.configs;
 
 import net.haraxx.coresystem.CoreSystem;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -19,14 +20,23 @@ public class WorldSpawnConfig {
 
     public void loadLocation() {
         FileConfiguration config = YamlConfiguration.loadConfiguration(new File(path));
-        spawnLocation = config.getLocation("firstSpawn");
-        worldSpawnLocation = config.getLocation("worldSpawn");
+        if (config.getString("firstSpawn") != null) {
+            spawnLocation = stringToLoc(config.getString("firstSpawn"));
+            worldSpawnLocation = stringToLoc(config.getString("worldSpawn"));
+            System.out.println("FOUND LOCATIONS");
+        } else {
+            spawnLocation = new Location(Bukkit.getServer().getWorld("test1"), 47, -50, 80);
+            worldSpawnLocation = new Location( Bukkit.getServer().getWorld( "test1" ), 3246, 114,  -9981);
+            System.out.println("DIDNT FOUND LOCATIONS");
+            System.out.println(spawnLocation);
+            saveLocations();
+        }
     }
 
     public void saveLocations() {
         FileConfiguration config = new YamlConfiguration();
-        config.set("firstSpawn", spawnLocation);
-        config.set("worldSpawn", worldSpawnLocation);
+        config.set("firstSpawn", locToString(spawnLocation));
+        config.set("worldSpawn", locToString(worldSpawnLocation));
         try {
             config.save(new File(path));
         } catch (Exception ignored) { }
@@ -48,6 +58,20 @@ public class WorldSpawnConfig {
     public void setWorldSpawnLocation(Location location) {
         this.worldSpawnLocation = location;
         saveLocations();
+    }
+
+    public String locToString(Location loc) {
+        int x = loc.getBlockX();
+        int y = loc.getBlockY();
+        int z = loc.getBlockZ();
+        float yaw = loc.getYaw();
+        float pitch = loc.getPitch();
+        return x+"/"+y+"/"+z+"/"+loc.getWorld().getName()+"/"+yaw+"/"+pitch;
+    }
+
+    public Location stringToLoc(String str) {
+        String[] parts = str.split("/");
+        return new Location(Bukkit.getServer().getWorld(parts[3]), Integer.parseInt(parts[0]), Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), Float.parseFloat(parts[4]), Float.parseFloat(parts[5]));
     }
 
     public static WorldSpawnConfig get() {
