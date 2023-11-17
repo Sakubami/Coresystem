d4a943bcfbc592532b66bdfe7db720d24e83298e
BuildTools <louiswaechter1994@gmail.com>
2023-11-17 03:55:44 +0100

    a

--- src/main/java/net/haraxx/coresystem/CoreSystem.java
+++ src/main/java/net/haraxx/coresystem/CoreSystem.java
@@ -1,13 +1,11 @@
 package net.haraxx.coresystem;
 
-import net.haraxx.coresystem.commands.CoreCommand;
-import net.haraxx.coresystem.commands.SpawnCommand;
-import net.haraxx.coresystem.commands.subcommands.*;
-import net.haraxx.coresystem.listener.PlaceStuffIdk;
-import net.haraxx.coresystem.listener.UnverifiedListener;
-import net.haraxx.coresystem.plugins.rpg.player.RPGPlayerConfig;
-import net.haraxx.coresystem.listener.PlayerSpawn;
+import net.haraxx.coresystem.api.command.CommandGroup;
+import net.haraxx.coresystem.commands.item.ItemGet;
+import net.haraxx.coresystem.commands.old.SpawnCommand;
 import net.haraxx.coresystem.configs.WorldSpawnConfig;
+import net.haraxx.coresystem.listener.*;
+import net.haraxx.coresystem.plugins.rpg.player.RPGPlayerConfig;
 import net.haraxx.coresystem.plugins.zoll.LocationConfig;
 import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
@@ -15,39 +13,101 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 
-public final class CoreSystem extends JavaPlugin {
+public final class CoreSystem extends JavaPlugin
+{
 
     private static CoreSystem instance;
 
+    private LocationConfig locationConfig;
     private RPGPlayerConfig rpgPlayerConfig;
     private WorldSpawnConfig worldSpawnConfig;
 
     @Override
-    public void onEnable() {
-        try {
+    public void onEnable()
+    {
+        try
+        {
             instance = this;
 
             //initiate Internal stuff
 
             //listener
-            Bukkit.getPluginManager().registerEvents(new PlayerSpawn(), this);
-            Bukkit.getPluginManager().registerEvents(new UnverifiedListener(), this);
+            Bukkit.getPluginManager().registerEvents( new PlaceStuffIdk(), this );
+            Bukkit.getPluginManager().registerEvents( new PlayerSpawn(), this );
+            Bukkit.getPluginManager().registerEvents( new UnverifiedListener(), this );
+
+            //commands
+
+            //create command groups
+            CommandGroup mainCommand = new CommandGroup( getCommand( "haraxx" ) );
+            CommandGroup itemCommands = new CommandGroup( "item" );
+
+            mainCommand.addSubCommand( itemCommands );
+
+            //add actual commands
+            itemCommands.addSubCommand( new ItemGet() );
+
+            //register main command group(s)
+            mainCommand.register();
+
+            //init core command
+//            PluginCommand rawCoreCommand = Objects.requireNonNull(getCommand("haraxx"));
+
+            //register core subcommands
+//            CoreCommand coreCoreCommand = new CoreCommand();
+//            coreCoreCommand.registerCoreSubCommand("item", new CMD_ITEM());
+//            coreCoreCommand.registerCoreSubCommand("spawn", new CMD_SPAWN());
+//            coreCoreCommand.registerCoreSubCommand("verify", new CMD_VERIFY());
+//            coreCoreCommand.registerCoreSubCommand("unverify", new CMD_UNVERIFY());
+//            coreCoreCommand.registerCoreSubCommand("rpg", new CMD_RPG());
+//            coreCoreCommand.registerCoreSubCommand("player", new CMD_PLAYER());
+
+            //register final command
+//            rawCoreCommand.setExecutor(coreCoreCommand);
+//            rawCoreCommand.setTabCompleter(coreCoreCommand);
+
+            PluginCommand spawnCommand = Objects.requireNonNull( getCommand( "spawn" ) );
+            spawnCommand.setExecutor( new SpawnCommand() );
 
             //init configs
+            locationConfig = new LocationConfig();
+            rpgPlayerConfig = new RPGPlayerConfig();
             worldSpawnConfig = new WorldSpawnConfig();
+
+            //saving stuff
+            //RPGPlayerConfig.get().autoSave();
+
             this.getLogger().addHandler( new LogTracker().onlyExceptions() );
-        } catch (Exception i) {
+        }
+        catch ( Exception i )
+        {
             i.printStackTrace();
         }
     }
 
     @Override
-    public void onDisable() {
+    public void onDisable()
+    {
+        System.out.println( " » disabling [ Core ]" );
     }
 
-    public static CoreSystem getInstance() {
+    public static CoreSystem getInstance()
+    {
         return instance;
     }
 
-    public WorldSpawnConfig getWorldSpawnLocation() { return worldSpawnConfig; }
+    public LocationConfig getLocationConfig()
+    {
+        return locationConfig;
+    }
+
+    public RPGPlayerConfig getRPGPlayerConfig()
+    {
+        return rpgPlayerConfig;
+    }
+
+    public WorldSpawnConfig getWorldSpawnLocation()
+    {
+        return worldSpawnConfig;
+    }
 }
--- src/main/java/net/haraxx/coresystem/builder/InventoryBuilder.java
+++ src/main/java/net/haraxx/coresystem/builder/InventoryBuilder.java
@@ -11,91 +11,92 @@ import java.util.ArrayList;
 public class InventoryBuilder {
 
     private final Inventory inventory;
-    private final ItemStack filler = new ItemBuilder(Material.LIGHT_GRAY_STAINED_GLASS_PANE, "§0").build();
+    private final int rows;
+    private final ItemStack filler = new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE).displayname("§3").build();
     private int math(int rows) {
         rows *= 9;
         if (rows> 54) rows = 54;
         return rows;
     }
 
-    public InventoryBuilder(int rows) {
-        this.inventory = Bukkit.createInventory(null, math(rows));
-    }
-
     public InventoryBuilder(int rows, String displayname) {
+        this.rows = rows;
         this.inventory = Bukkit.createInventory(null, math(rows), displayname);
-    }
-
-    public InventoryBuilder setFunction(int slot, String function) {
-        inventory.getItem(slot).getItemMeta().setLocalizedName(function);
-        return this;
     }
 
     public InventoryBuilder buildBottomRow() {
         for (int i = inventory.getSize()-9; i < inventory.getSize(); i++) {
-            inventory.setItem(i, new ItemBuilder(Material.GRAY_STAINED_GLASS_PANE, "§0").build());
+            inventory.setItem(i, new ItemBuilder(Material.BLACK_STAINED_GLASS_PANE, "§0").build());
         }
-        inventory.setItem(inventory.getSize()-5, new ItemBuilder(Material.BARRIER, "§cSchließen").nbt( "protected", "true" ).build());
+        inventory.setItem(inventory.getSize()-5, new ItemBuilder(Material.BARRIER, "§cSchließen").build());
         return this;
     }
 
     public InventoryBuilder fillEmpty(ItemStack itemStack) {
         for (int i = 0; i < inventory.getSize(); i++)
-            if (inventory.getItem(i) == null) {
-                inventory.setItem(i, itemStack);
-            }
-        return this;
-    }
-
-    public InventoryBuilder fillEmpty() {
-        fillEmpty(filler);
+            if ( itemStack != null ) {
+                if (inventory.getItem(i) == null) {
+                    inventory.setItem(i, itemStack);
+                }
+            } else { fillEmpty( filler ); }
         return this;
     }
 
     public InventoryBuilder setItem(ItemStack item, int slot) {
-        this.inventory.setItem(slot, item);
+        if ( item != null )
+            this.inventory.setItem(slot, item);
         return this;
     }
 
     public InventoryBuilder addItem(ItemStack item) {
-        this.inventory.addItem(item);
-        return this;
-    }
-
-    public InventoryBuilder fillRow(int row) {
-        return setRow(row, filler, false);
-    }
-
-    public InventoryBuilder setRow(int row, ItemStack item, boolean overwrite) {
-        for (int i = (row * 9)- 9; i < (row * 9 )- 1; i++) {
-            if (overwrite) {
-                this.inventory.setItem(i, item);
-            } else if (this.inventory.getItem(i) == null) {
-                this.inventory.setItem(i, item);
-            }
-        }
-        return this;
-    }
-
-    public InventoryBuilder fillColumn(int row) {
-        return setColumn(row, filler, false);
-    }
-
-    public InventoryBuilder setColumn(int column, ItemStack item, boolean overwrite) {
-        for (int i = (column * 9)- 1; i < (column * 9 )- 9; i+=9) {
-            if (overwrite) {
-                this.inventory.setItem(i, item);
-            } else if (this.inventory.getItem(i) == null) {
-                this.inventory.setItem(i, item);
-            }
-        }
+        if ( item != null )
+            this.inventory.addItem(item);
         return this;
     }
 
     public InventoryBuilder addItems(ArrayList<ItemStack> items) {
-        for (ItemStack item : items) {
-            this.inventory.addItem(item);
+        if ( !items.isEmpty() ) {
+            for (ItemStack item : items) {
+                this.inventory.addItem(item);
+            }
         }
+        return this;
+    }
+
+    public InventoryBuilder setRow(int row, ItemStack item, boolean overwrite) {
+        if ( item != null ) {
+            for (int i = (row * 9)- 9; i < (row * 9); i++) {
+                if (overwrite) {
+                    this.inventory.setItem(i, item);
+                } else if (this.inventory.getItem(i) == null) {
+                    this.inventory.setItem(i, item);
+                    System.out.println("HEY IM A DEBUG ------------ " + i);
+                }
+            }
+        }   
+        return this;
+    }
+
+    public InventoryBuilder fillRow( int row ) {
+        setRow( row , filler, false );
+        return this;
+    }
+
+    public InventoryBuilder setColumn(int column, ItemStack item, boolean overwrite) {
+        if ( item != null ) {
+            for (int i = column -1; i < (rows * 9); i+=9) {
+                if (overwrite) {
+                    this.inventory.setItem(i, item);
+                } else if (this.inventory.getItem(i) == null) {
+                    this.inventory.setItem(i, item);
+                }
+            }
+        }
+        return this;
+    }
+
+    public InventoryBuilder fillColumn( int column ) {
+        setColumn( column , filler, false );
         return this;
     }
 
